9710287026c7acb339ecb0d4842e297f3d589674
juliettegigi <mar9ina@gmail.com>
2023-08-11 17:02:28 -0300

    prree

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,7 @@ const btnSalir=document.getElementById("btnSalir");
 
 var url = ( window.location.hostname.includes('localhost') )
                     ? 'http://localhost:8080/api/auth/'
-                    : 'https://restserver-curso-fher.herokuapp.com/api/auth/google';/* TODO cambiar esto */
+                    : 'https://chattsockett.onrender.com/api/auth';/* TODO cambiar esto */
 
 let usuario=null;
 let socket=null;
